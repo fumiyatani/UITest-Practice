b55481d5f23967b1f42186e733f2ad905fd493b8
tanifumiya <fumiyadeveloper@gmail.com>
2022-05-28 23:14:51 +0900

    first commit

---
+++ app/src/main/java/com/ftani/uitest_practice/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.ftani.uitest_practice.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
